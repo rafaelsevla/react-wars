7ce13f3b23fedb97defed886d530539fa15ba5b9
Rafael Costa <rafaelsevla@gmail.com>
2020-03-22 02:47:17 -0300

    Updates

---
+++ precache-manifest.29dac42aeae4c5e9ef716b4e92efc8cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63618153cc57d46db925dfa474d610e4",
+    "url": "/react-wars/index.html"
+  },
+  {
+    "revision": "823dda7c7684907c24f3",
+    "url": "/react-wars/static/css/2.9c817a2e.chunk.css"
+  },
+  {
+    "revision": "823dda7c7684907c24f3",
+    "url": "/react-wars/static/js/2.683a5751.chunk.js"
+  },
+  {
+    "revision": "2875251cdadf114299c43411dbc2e85b",
+    "url": "/react-wars/static/js/2.683a5751.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6538b2f289ef1a275245",
+    "url": "/react-wars/static/js/3.b10cd759.chunk.js"
+  },
+  {
+    "revision": "3fc7a8afc337456e6eb73316df3df069",
+    "url": "/react-wars/static/js/3.b10cd759.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f8ece06488952cbce10",
+    "url": "/react-wars/static/js/4.a38bee80.chunk.js"
+  },
+  {
+    "revision": "249f3f72147c13d375a0",
+    "url": "/react-wars/static/js/main.c8e7e292.chunk.js"
+  },
+  {
+    "revision": "3c071328a9a37c2e2aeb",
+    "url": "/react-wars/static/js/runtime-main.4fb939a8.js"
+  }
+]);
